c191c3af4e6548e65198fedc5df660be5f962f92
aatxegorri <a.gilbert416@gmail.com>
2021-05-25 20:39:24 +1200

    jest added

--- search-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders subheader', () => {
-  render(<App />);
-  const linkElement = screen.getByText("CISE - Team 21");
-  expect(linkElement).toBeInTheDocument();
-});
